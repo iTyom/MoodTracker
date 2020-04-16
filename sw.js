07ae001b5fdfb0d08bff39d3b79ebca59527f4fb
Tom Ruscelli <tom.ruscelli@ynov.com>
2020-04-16 15:38:20 +0200

    try fix pwa

--- sw.js
+++ sw.js
@@ -2,15 +2,11 @@ self.addEventListener('install', e => {
     e.waitUntil(
         // Après l'installation du service worker,
         // ouvre un nouveau cache
-        caches.open('mon-cache-pwa').then(cache => {
+        caches.open('pwa-cache').then(cache => {
             // Ajoute toutes les URLs des éléments à mettre en cache
             return cache.addAll([
                 '/',
                 '/index.html',
-                '/about.html',
-                '/images/doggo.jpg',
-                '/styles/main.min.css',
-                '/scripts/main.min.js',
             ]);
         })
     );
