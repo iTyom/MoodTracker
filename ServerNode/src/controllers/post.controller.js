b400944e353e5496356a43d32f2327f21348e9c6
damienne broihier <ludovic.robez@hotmail.fr>
2020-05-27 16:23:25 +0200

    Add Post Controller

---
+++ ServerNode/src/controllers/post.controller.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('../../_helpers/jwt');
+const db = require('../../_helpers/db');
+
+
+// routes
+router.post('/addPost', jwt.isAuthorized, addPost);
+router.get('/getPosts', jwt.isAuthorized, getPosts);
+router.get('/getPostsByAllegiance', jwt.isAuthorized, getPostsByAllegiance);
+
+
+module.exports = router;
+
+function getPosts(req, res, next) {
+    db.Post.find({ 'postedBy._id': req.params.userId })
+    .then(data => data ? res.json(data) : res.status(400).json({
+        message: "Erreur"
+    }))
+    .catch(err => next(err));
+}
+
+function getPostsByAllegiance(req, res, next) {
+    db.Post.find({ 'postedBy._id': req.params.userId, 'allegiance': req.params.allegiance })
+    .then(data => data ? res.json(data) : res.status(400).json({
+        message: "Erreur"
+    }))
+    .catch(err => next(err));
+}
+
+function addPost(req, res, next) {
+    db.User.findOne({'_id': req.params.userId })
+    .then(user => {
+        if (user){
+            var post = new db.Post({ text: req.params.text, allegiance: req.params.allegiance , postedBy: user });
+            post.save()
+            .then(data => data ? res.status(201).json(post) : res.status(400).json({
+                message: "Erreur"
+            }))
+            .catch(err => next(err));
+        } else {
+            res.status(400).json({
+                message: "Erreur"
+            })
+        }
+      })
+    .catch(err => next(err));
+}
