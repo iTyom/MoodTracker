986df7b287fe351468d0e87e5851ed74fd55f430
Tom Ruscelli <tom.ruscelli@ynov.com>
2020-05-28 13:44:10 +0200

    scrolling

--- ClientAngular/src/pages/allegiance/allegiance.component.ts
+++ ClientAngular/src/pages/allegiance/allegiance.component.ts
@@ -11,49 +11,22 @@ import { AuthService } from 'src/services/auth.service';
 })
 export class AllegianceComponent implements OnInit {
 
-  private posts: Post;
-  public demons: [];
-  public angels:[];
-  public ratioAngels: int;
-  public ratioDemons: int;
+  public posts: Post;
 
   constructor(private socketService: SocketService, private postService: PostService, private authService: AuthService) { }
 
   ngOnInit() {
     this.init();
-    console.log('saluuuuuuuuuuuuut');
   }
 
   async init() {
     await this.getPosts();
-    await this.getDemonMessages();
-    await this.getAngelMessages();
-    console.log('coucou');
   }
 
   async getPosts() {
-    console.log('3');
-    const response = await this.postService.getPostsByAllegience('demon').toPromise();
+    const response = await this.postService.getPosts().toPromise();
     if (response) {
-      console.log("AllegianceComponent -> getPosts -> response", response);
-
-      this.posts = response;
     }
   }
 
-  async getDemonMessages()
-  {
-      this.demons = this.posts.filter(a => a.allegiance === 'demon');
-
-      this.ratioDemons = (this.demons.length/this.posts.length) * 100
-
-
-  }
-
-  async getAngelMessages()
-  {
-      this.angels = this.posts.filter(a => a.allegiance === 'ange');
-
-      this.ratioAngels = (this.angels.length/this.posts.length) * 100
-  }
 }
